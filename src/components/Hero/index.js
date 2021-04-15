dd96b2bcac5ae2374a00d5a35d077eac75f39115
Carlos Abril <carlos.abril87@outlook.com>
2021-04-15 09:50:18 -0400

    navbar

---
+++ src/components/Hero/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css"
+
+
+
+function Hero() {
+    
+    return (
+                  
+        <section class="bgimage">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    )
+}
+
+export default Hero;
